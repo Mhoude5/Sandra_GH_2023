4d1d2bb03785d044ff60476b7025221a33f980b5
Maddie Houde <Maddie.Houde@usu.edu>
2023-03-29 10:41:18 -0600

    Re worked analysis post input frmo Susan. Also re-worked custom contrast script. I think we are done looking at this data, and mostly have to visualize via graphs at this point

---
+++ Model_Custom-Contrasts.R
@@ -0,0 +1,103 @@
+# Germination Counts Model Analysis Script
+# for 2023 Greenhouse Undergraduate Research Experiment
+# Using a model we selected in 'Model_Germ-Counts.R'
+
+# Load packages
+library(emmeans)
+library(glmmTMB)
+library(car)
+library(DHARMa)
+library(blme)
+
+
+getwd()
+setwd("~/Experiments_R/Sandra_GH_2023/Processed_Data")
+list.files()
+load("Model_object.RData") # Load model object here
+load("DF_Germination-Fresh-25-Only.RData") # Load dataframe here
+
+
+# Here we will specify a comparison between model estimates of the reduction
+# in germination between Fresh and 25, across Species
+# Which interaction is significant?
+
+# Customized contrasts (the lengthy way)----
+
+summary(G_Mod2)
+emmeans(G_Mod2, pairwise ~ Salinity | Species)$contrasts 
+#these contrasts are what we will create
+
+emm <- emmeans(G_Mod2, ~ Species*Salinity)
+emm
+
+# Estimate means for specific treatment combinations
+#   Should match emmeans output
+# You create a vector where 1 = the emmean estimate for the species you want
+# and 0's for the rest (it will only save the desired mean)
+# for ALOC_Fresh, you would do c(1, 0, 0, 0, 0, 0)
+# This is equivalent to c(4.22, 0, 0, 0, 0, 0)
+# Because it is first in our summary, and we have 6 parameters total.
+
+custom1 <- list(`ALOC_Fresh` = c(1, 0, 0, 0, 0, 0), 
+                `ALOC_25`     = c(0, 0, 0, 1, 0, 0),
+                `PHAU_Fresh`  = c(0, 1, 0, 0, 0, 0),
+                `PHAU_25`     = c(0, 0, 0, 0, 1, 0),
+                `SARU_Fresh`  = c(0, 0, 1, 0, 0, 0),
+                `SARU_25`     = c(0, 0, 0, 0, 0, 1)
+                )
+contrast(emm, custom1)
+emmeans(G_Mod2, ~ Species*Salinity)
+# Note these match, but with order changed to our specification.
+
+
+# Now estimate pairwise comparisons of Salinity effects for each Species
+#   Note that for Fresh - 25, we subtract 25 coefficients from Fresh coefficients
+#   Should match emmeans pairwise output
+custom2 <- list(`ALOC_Fresh - ALOC_25` = c(1, 0, 0, -1,  0, 0), 
+                `PHAU_Fresh - PHAU_25`  = c(0, 1, 0,  0, -1, 0),
+                `SARU_Fresh - SARU_25`  = c(0, 0, 1,  0, 0, -1))
+contrast(emm, custom2)
+emmeans(G_Mod2, pairwise ~ Salinity | Species)$contrasts
+
+# Now estimate interaction of Species (ALOC & PHAU & SARU) and Salinity (Fresh & 25)
+#   Again, subtracting coefficients
+custom3 <- list(`ALOC_Salinity - PHAU_Salinity` = c(1, -1, 0, -1, 1, 0),
+                `ALOC_Salinity - SARU_Salinity` = c(1, 0, -1, -1, 0, 1),
+                `PHAU_Salinity - SARU_Salinity` = c(0, 1, -1, 0, -1, 1)
+                )
+contrast(emm, custom3)
+# You can check this by hand:
+#   ALOC_Salinity = 1.90
+#   PHAU_Salinity = 5.44
+#   SARU_Salinity = 4.25
+#   ALOC - PHAU = -3.54
+#   ALOC - SARU = -2.35
+#   PHAU - SARU = 1.19
+
+# Note p values. As expected, PHAU experienced significantly more reduction in 
+# germination than ALOC from the freshwater to 25 ppt level.
+# No other interaction is significant.
+
+
+# Customized contrasts (the short way)----
+
+emm1 <- emmeans(G_Mod2, ~ Salinity | Species) 
+emm1
+con1 <- contrast(emm1, interaction = "pairwise")
+con1
+pairs(con1, by = NULL)
+# Note that the pairs() is a tukey pairwise estimate
+# Some notes from Susan --
+
+    # Many useful contrast methods are available other than "pairwise"
+    #   See documentation for Contrast families
+    #   contrast-methods {emmeans}
+    
+    # If custom2 or custom3 had multiple contrasts with re-use of means,
+    #   you can add  adjust = "tukey"  to contrast()
+
+# However, the numbers here match what we did earlier, and the p values are more
+# conservative, but the story remains the same
+# the significant diff here is between ALOC and PHAU. 
+# YAY!
+
--- Model_Germ-Counts.R
+++ Model_Germ-Counts.R
@@ -1,6 +1,13 @@
-# Germination Counts Model for 2023 Greenhouse Undergraduate Research Experiment
+# Germination Counts Model Creation Script
+# for 2023 Greenhouse Undergraduate Research Experiment
 
 # Load packages ----
+# Sandra- you will probably have to install these, you can do so in the bottom 
+# right (click on packages tab, and then install)
+# or you can install directly in the console (copy and paste, ...
+# dont run it in the script, as you don't want to re-install every time..)
+# install.packages("package name")
+
 library(tidyverse) #gives ggplot2 and piping
 library(glmmTMB)
 library(car)
@@ -22,6 +29,7 @@ Germ <- Germ %>%
 which(is.na(Germ$Total_Emergence))
 
 # See a short summary table of total counts, added across reps ----
+# (We wont analyze this table, it's just for me to quickly see the data..)
 Summary <- Germ %>%
   dplyr::group_by(Species, Salinity) %>%
   dplyr::summarise(Total_Emergence = sum(Total_Emergence)) %>%
@@ -30,6 +38,8 @@ Summary <- Germ %>%
 # ALOC has counts at Fresh and 25, but not 40 PPT
 # PHAU has counts at Fresh and 25, but not 40 PPT
 # SARU has counts at Fresh, but not 25 or 40.
+
+
 
 # Column combined with Row equates to the whole plot location (i.e. a replicate)
 # emergence are integer counts of seedlings, we removed seedlings as we counted them
@@ -56,9 +66,6 @@ G_Mod <- glmmTMB(Total_Emergence ~ Species + Salinity + Species:Salinity
             data = Germ)
 # Model convergence problem
 
-
-
-
 # Remove 40PPT values, as we have 0's across the board
 # and we recognize this as too high of a salinity
 # and not worth analyzing.
@@ -68,10 +75,12 @@ Germ1 <- Germ %>%
 Germ1$Salinity <- factor(Germ1$Salinity)
 levels(Germ1$Salinity) # re-factor salinity since we removed 40
 
-# Another way to refactor (less intuitive for Maddie's brain)
+# Another way to re-factor (less intuitive for Maddie's brain)
 Germ1 <- Germ %>%
   dplyr::filter(Salinity %in% c("Fresh", "25")) %>%
   dplyr::mutate(Salinity = factor(Salinity)) # re-factor salinity since we removed 40
+
+
 
 
 # Poisson Model Fresh & 25 only ----
@@ -79,11 +88,13 @@ G_Mod1 <- glmmTMB(Total_Emergence ~ Species + Salinity + Species:Salinity
                  +  (1 | Whole_Plot),
                  family = poisson, 
                  data = Germ1)
-summary(G_Mod1)
+OG_summ <- print(summary(G_Mod1)) # Saving this summary for future comparison
 
 G_Mod1.resid <- simulateResiduals(G_Mod1, plot=T)
 plotResiduals(G_Mod1.resid, form = Germ1$Salinity)
 # Diagnostic plots could be improved
+
+
 
 
 # Quasi Poisson Fresh and 25 only ----
@@ -93,93 +104,131 @@ G_Mod1 <- glmmTMB(Total_Emergence ~ Species + Salinity + Species:Salinity
                   data = Germ1)
 summary(G_Mod1)
 G_Mod1.resid <- simulateResiduals(G_Mod1, plot=T)
-
 # Spread of residuals is improved using this model 
 
-
-## Explore model ----
-plotResiduals(G_Mod1.resid, form = Germ1$Salinity)
-
-
-emmip(G_Mod1, Species ~ Salinity, CIs = F) # salinity effect
-# issue with SARU
-
-emmip(G_Mod1, Salinity ~ Species, CIs = T, type = "response") # Species effect
-# issue with SARU
-
-emmip(G_Mod1, Salinity ~ Species, CIs = F)
-
-car::Anova(G_Mod1) #significant interaction Species:Salinity
-emmeans(G_Mod1,  ~ Salinity | Species, type = "response") # back transformed
-
-emmeans(G_Mod1, pairwise ~ Salinity | Species) # on log scale
-emmeans(G_Mod1, ~ Salinity | Species) 
-# customized contrasts would match these vals (Fresh - 25)
-# huge standard error for SARU that must be fixed
-
-# Customized contrasts ----
-# (I have to fix model first, but practicing contrasts here)
-
-emm1 <- emmeans(G_Mod1, ~ Salinity | Species) 
-emm1
-con1 <- contrast(emm1, interaction = "pairwise")
-con1
-pairs(con1, by = NULL)
-
-
-# Original method I tried to test contrasts below
-alocF <- c(1, rep(0, 5))
-aloc25 <- c(0, 1, rep(0, 4))
-PHAUF <- c(0, 0, 1, rep(0, 3))
-PHAU25 <- c(rep(0, 3), 1, 0, 0)
-SARUF <- c(rep(0, 4), 1, 0)
-SARU25 <- c(rep(0, 5), 1)
-
-
-alocRED <- alocF - aloc25
-phauRED <- PHAUF - PHAU25
-saruRED <- SARUF - SARU25
-
-contrast(emm1, method = list("ALOC to PHAU" = alocRED - phauRED,
-                             "PHAU to SARU" = phauRED - saruRED,
-                             "ALOC to SARU" = alocRED - saruRED) ) 
-# non conforming, I am pretty sure this has to do with the EMMgrid object
-# being separated by species rather than a single column,
-# it may be acting more like an array so my c(0,0,0,0,0,0) objects wont work
-# but unsure how to fix it.
+    ## Explore model ----
+    plotResiduals(G_Mod1.resid, form = Germ1$Salinity)
+  
+    emmip(G_Mod1, Species ~ Salinity, CIs = F) # salinity effect
+    # note issue with SARU
+  
+    emmip(G_Mod1, Salinity ~ Species, CIs = T, type = "response") # Species effect
+    # note issue with SARU
+  
+    emmip(G_Mod1, Salinity ~ Species, CIs = F) # You can toggle CI's on and off
+  
+    # Check for significance of fixed effects
+    car::Anova(G_Mod1) #significant interaction Species:Salinity
+    
+    emmeans(G_Mod1,  ~ Salinity | Species, type = "response") # back transformed
+    emmeans(G_Mod1, pairwise ~ Salinity | Species) # on log scale & pairwise
+    emmeans(G_Mod1, ~ Salinity | Species) # Just by species (not pairwise)
+    # customized contrasts would match these vals (Fresh - 25)
+    # huge standard error for SARU that must be fixed
 
 
 # Model with fixed effects priors ----
-## Poisson fixed effects priors ----
+    
+# We have 6 fixed effects to add priors to (including intercept), you can see this in
+    
+## Model with reasonable priors (3) ----
 G_Mod1 <- bglmer(Total_Emergence ~ Species * Salinity
                   +  (1 | Whole_Plot),
-                  family = poisson(),
-                  fixef.prior = normal(sd = c(3, 3, 3)), 
+                  family = poisson(), # back to Poisson for now
+                  fixef.prior = normal(sd = c(3, 3, 3, 3, 3, 3)), 
                   data = Germ1)
+# if you don't specify enough parameters in c(# # #), the intercept is taken separate
+# and the tail is repeated. 
+# for example c(10, 2.5) would have a SD of 10 on the intercept, and 2.5 for remaining
+# parameters
                 
+# Notice the change in SE's from OG_summ to line 139.
+OG_summ
 summary(G_Mod1)
-G_Mod1.resid <- simulateResiduals(G_Mod1, plot=T)
-# Diagnostic plots could be improved, go to nbinom
 
-## Neg binom fixed effects prior ----
+
+## Model with default normal priors ----
 G_Mod1 <- bglmer(Total_Emergence ~ Species * Salinity
                  +  (1 | Whole_Plot),
-                 family = nbinom2(),
-                 # family = nbinom1(), 
-                 # nbinom1() error: "Downdated VtV is not positive definite"
-                 fixef.prior = normal(sd = c(3, 3, 3)), 
+                 family = poisson(),
+                 fixef.prior = normal(), # going with defaults
                  data = Germ1)
-summary(G_Mod1) # vcov issue
-# Diagnostic plots won't run for nbinom2(). How to evaluate ?
-G_Mod1.resid <- simulateResiduals(G_Mod1, plot=T)
 
-## Neg binom, fixed effects on Species but not salinity ----
-G_Mod1 <- bglmer(Total_Emergence ~ Species * Salinity
+summary(G_Mod1)
+# You can see in the summary that default is c(10, 2.5)
+# The SE is slightly smaller for defaults than for our original vals c(3, 3)
+
+
+
+## Model with weak penalization (1e4) for all but the interaction (2.5) ----
+G_Mod2 <- bglmer(Total_Emergence ~ Species * Salinity
                  +  (1 | Whole_Plot),
-                 family = nbinom2(),
-                 fixef.prior = normal(sd = c(3, Inf, 3)), 
+                 family = poisson(),
+                 fixef.prior = normal(c(1e4, 1e4, 1e4, 1e4, 2.5, 2.5)), 
                  data = Germ1)
 
-summary(G_Mod1) #vcov issue ?
+# Note that our standard error estimates seem pretty reasonable, again compare to OG
+# Also notice only the interaction terms estimate really changed 
+# (SpeciesPHAU:Salinity25, SpeciesSARU:Salinity25)
+OG_summ
+summary(G_Mod2)
+# I like this fixed effects prior the best, because we only penalize the interaction
+# I don't think we have to penalize our main effects since they are so strong.
+# But I will mention I am no expert at this, and Susan might have a more nuanced
+# view of this particular model.
+
+    ## Explore model ----
+    G_Mod2.resid <- simulateResiduals(G_Mod2, plot=T) # warning message DHARMa
+
+    plotResiduals(G_Mod2.resid, form = Germ1$Salinity)
+    # Note difference in variance.. Remember we first fixed this by moving
+    # to a nbinom1() distribution. However, that is not an option with fixef.priors,
+    # so we have to accept that our poisson model is perhaps not the best fit,
+    # but the best we can do in this scenario.
+    
+    emmip(G_Mod2, Species ~ Salinity, CIs = T) # salinity effect
+    # much more reasonable compared to first attempt
+    # Which interactions would you expect to be different here?
+    # Remember interactions take parallel lines and make them cross at some point.
+    # because it shifts the slope of the line
+    # MY guess would be ALOC and PHAU are different
+    # but not SARU and PHAU, and not SARU and ALOC.
+    
+    emmip(G_Mod2, Salinity ~ Species, CIs = T, type = "response") # Species effect
+    # note difference in CI bars between Fresh and 25
+    emmip(G_Mod2, Salinity ~ Species, CIs = F, type = "response")
+    
+    # Check for significance of fixed effects
+    car::Anova(G_Mod2) #significant interaction Species:Salinity
+    # Note that the interaction is a LOT more significant than our first model now that
+    # we have fixed the SARU estimate
+    
+    emmeans(G_Mod2,  ~ Salinity | Species, type = "response") # back transformed
+    # i.e., our actual germination rate estimates (~66 seeds at Fresh for PHAU)
+    emmeans(G_Mod2, pairwise ~ Salinity | Species) # on log scale & pairwise
+    # customized contrasts would match these vals (Fresh - 25)
+    emmeans(G_Mod2, ~ Salinity | Species) # Just by species (not pairwise)
+
+# Check diagnostics
+G_Mod2.resid <- simulateResiduals(G_Mod2, plot=T)
+# Diagnostic plots could be improved
+# Per Susan: Lots of problems with these data for other distributions -
+# e.g., negative binomal nbinom1() or nbinom2()
+# We can try a different package (brms), BUT the story of our data is pretty clear
+# I.E.- we dont have to go crazy in our analysis given our clear story through graphs
+# and my limited understanding of fixed effects priors. 
+# (It is hard to justify a complex analysis that I don't fully understand)
+
+
+# Let's call it a day, save the model object and move on.
+getwd()
+setwd("~/Experiments_R/Sandra_GH_2023/Processed_Data")
+save(G_Mod2, file = "Model_object.RData")
+save(Germ1, file = "DF_Germination-Fresh-25-Only.RData")
+list.files()
+
+# From here, we will further investigate our selected model ... 
+# Move to the Model_Custom-Contrasts R Script
+
 
 
